d5644445692504926f5f86ee4b0708928533d0e2
ntargon <argon19951128@gmail.com>
2023-01-22 08:28:59 +0900

    flowbiteを追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
   import Greet from "$lib/Greet.svelte";
+  import { Alert } from 'flowbite-svelte';
 </script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+	</Alert>
+</div>
 
 <div class="row">
   <Greet />
